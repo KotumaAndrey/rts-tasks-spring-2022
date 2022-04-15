b98551fbd4237fe70924f234733104800173fe9b
KotumaAndrey <73333613+KotumaAndrey@users.noreply.github.com>
2022-04-15 08:16:29 +0400

    Update task05-obesrver.ino

--- task05/task05-obesrver.ino
+++ task05/task05-obesrver.ino
@@ -1,9 +1,13 @@
+String getValuesCommand = "<< GET-VALUES <<";
+
+// Отправка команды на получение значений при запуске
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("START");
+    Serial.print(getValuesCommand);
 }
 
+// Вывод всех полученных значений
 void loop()
 {
     while(Serial.available()) {
